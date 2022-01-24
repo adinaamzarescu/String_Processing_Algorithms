8f9105f8dfed3d434a610e85669dde6cdffd81b9
adinaamzarescu <73987572+adinaamzarescu@users.noreply.github.com>
2022-01-24 16:15:21 +0200

    Add files via upload

---
+++ Tema2/Task.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public abstract class Task {
+    String inFilename;
+    String oracleInFilename;
+    String oracleOutFilename;
+    String outFilename;
+
+    public abstract void solve() throws IOException, InterruptedException;
+
+    public abstract void readProblemData() throws IOException;
+
+    public void formulateOracleQuestion() throws IOException {}
+
+    public void decipherOracleAnswer() throws IOException {}
+
+    public abstract void writeAnswer() throws IOException;
+
+    /**
+     * Stores the files paths as class attributes.
+     *
+     * @param inFilename         the file containing the problem input
+     * @param oracleInFilename   the file containing the oracle input
+     * @param oracleOutFilename  the file containing the oracle output
+     * @param outFilename        the file containing the problem output
+     */
+    public void addFiles(String inFilename, String oracleInFilename, String oracleOutFilename, String outFilename) {
+        this.inFilename = inFilename;
+        this.oracleInFilename = oracleInFilename;
+        this.oracleOutFilename = oracleOutFilename;
+        this.outFilename = outFilename;
+    }
+
+    public void askOracle() throws IOException, InterruptedException {
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.redirectErrorStream(true);
+        builder.command("python3", "sat_oracle.py", "sat.cnf", "sat.sol");
+        Process process = builder.start();
+        BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String buffer;
+        StringBuilder output = new StringBuilder();
+
+        while ((buffer = in.readLine()) != null) {
+            output.append(buffer).append("\n");
+        }
+
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            System.err.println("Error encountered while running oracle");
+            System.err.println(output.toString());
+            System.exit(-1);
+        }
+    }
+}
